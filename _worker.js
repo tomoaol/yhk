58bd6a3fe2bb490529e55455d73ae18109c24535
tomoaol <137114327+tomoaol@users.noreply.github.com>
2024-11-02 01:44:44 -0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'f9e87027-27ad-417d-b579-177685e9a461';
 
-let proxyIP = '43.154.230.30';
+let proxyIP = 'hk.luton.eu.org';
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
